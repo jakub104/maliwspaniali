c8e7e3cbc30cf181ad84c8489dfff8a08c8fa206
jakub104 <jakubzlezena@wp.pl>
2021-07-26 22:16:30 +0200

    Inicial commit

---
+++ layouts/default.vue
@@ -0,0 +1,97 @@
+<template>
+	<div class="default">
+		<main class="default__view">
+			<Nuxt />
+		</main>
+		<footer class="default__footer">
+			<div class="footer__contact">
+				<div class="contact__item" @click="call">
+					<fa class="item__icon" icon="phone" />603 792 353
+				</div>
+				<div class="contact__item" @click="writeMail">
+					<fa class="item__icon" icon="envelope" />maliwspaniali@wp.pl
+				</div>
+			</div>
+			<span class="footer__description">Prywatne Przedszkole "Mali Wspaniali" w Częstkowie &copy; 2021</span>
+		</footer>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "default",
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			call() {
+				window.open('tel:603792353')
+			},
+			writeMail() {
+				window.open('mailto:maliwspaniali@wp.pl')
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.default {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		.default__view {
+			flex-basis: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+		}
+		.default__footer {
+			padding: 20px;
+			display: flex;
+			flex-direction: column;
+			align-items: space-between;
+			justify-content: center;
+			text-align: center;
+			box-shadow:
+				75vw -5px 0 $green,
+				50vw -5px 0 $blue,
+				25vw -5px 0 $yellow,
+				0vw -5px 0 $red;
+			.footer__description {
+				margin-top: 20px;
+			}
+			.footer__contact {
+				width: min(1000px, 100%);
+				margin: 0 auto;
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: space-evenly;
+				.contact__item {
+					text-align: center;
+					font-size: 18px;
+					font-weight: bold;
+					letter-spacing: 1px;
+					transition: 0.3s ease;
+					margin: 5px;
+					.item__icon {
+						transition: 0.3s ease;
+						margin-right: 10px;
+					}
+					@media (min-width: 640px) {
+						width: 250px;
+					}
+					@include hover {
+						letter-spacing: 1.5px;
+						.item__icon {
+							transform: scale(1.2) rotate(-5deg);
+						}
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,91 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Mali Wspaniali',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'description', content: 'Prywatne Przedszkole "Mali Wspaniali" w Częstkowie. Otwarcie już wkrótce!' },
+      { name: 'keywords', content: 'Mali Wspaniali, przedszkole, prywatne, niepubliczne, Częstkowo' },
+      { name: 'author', content: 'Ewa Naczk' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/styles/global.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    {
+      src: '~/plugins/fragment.js',
+      ssr: false,
+    },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/fontawesome'
+  ],
+
+  fontawesome: {
+    component: 'fa',
+    icons: {
+      solid: true,
+      brands: true,
+    },
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    '@nuxtjs/style-resources',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    meta: {
+      title: 'Mali Wspaniali',
+      author: 'Ewa naczk',
+    },
+    manifest: {
+      name: 'Prywatne Przedszkole "Mali Wspaniali" w Częstkowie',
+      short_name: 'Mali Wspaniali',
+      description: 'Prywatne Przedszkole "Mali Wspaniali" w Częstkowie',
+      lang: 'pl',
+    },
+    icon: {
+      sizes: [64, 120, 144, 152, 192, 384, 512],
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    styleResources: {
+      scss: './styles/_*.scss',
+    },
+    extractCSS: true,
+  },
+}
